5d5ce27e71ebfb23fa7e4fc58309fdd071bc0ddb
Gerardo1909 <gerardotoboso1909@gmail.com>
2026-01-15 18:55:57 -0300

    refactor(src/extract/extractor.py): Se refactoriza módulo de extracción para que interactue con S3.

--- src/extract/extractor.py
+++ src/extract/extractor.py
@@ -2,27 +2,30 @@
 Módulo con lógica de extracción de API.
 """
 
-from pathlib import Path
 from typing import Dict, Any, List, Optional
-
-from extract.storage import RawStorage
 from extract.http_client import RequestsHttpClient
 
 
 class Extractor:
     """
     Clase que se encarga de la extracción de información
-    dentro del flujo ETL.
+    dentro del flujo ETL, guardando directamente en S3 usando S3IO (pyarrow+boto3).
     """
 
-    def __init__(self, http_client: RequestsHttpClient, storage: RawStorage):
+    def __init__(
+        self,
+        http_client: RequestsHttpClient,
+        IO_operator,
+        raw_data_dir: str,
+    ):
         self.client = http_client
-        self.storage = storage
+        self.IO_operator = IO_operator
+        self.raw_data_dir = raw_data_dir
 
-    def extract(self) -> Dict[str, Path]:
+    def extract(self) -> Dict[str, str]:
         """
-        Extrae y guarda todas las tablas de interés.
-        Retorna un diccionario con los paths de los archivos guardados.
+        Extrae y guarda todas las tablas de interés como Parquet en S3 usando pyarrow.
+        Retorna un diccionario con los paths S3 de los archivos guardados.
         """
         payload = self.client.get()
         saved_files = {}
@@ -30,12 +33,12 @@ class Extractor:
         for table_name in payload.get("tables", {}).keys():
             table_data = self._get_table_from_payload(payload, table_name)
             if table_data:
-                file_path = self.storage.save(
-                    file_name=table_name,
-                    payload=table_data,
-                )
-                saved_files[table_name] = file_path
+                s3_path = f"{self.raw_data_dir}/{table_name}/"
+                self.IO_operator.save_parquet(payload=table_data, s3_path=s3_path)
+                saved_files[table_name] = s3_path
 
+        if not saved_files:
+            raise ValueError("No se encontraron tablas para extraer en el payload.")
         return saved_files
 
     def _get_table_from_payload(
@@ -49,9 +52,12 @@ class Extractor:
 
 
 if __name__ == "__main__":
-    from config.path_config import RAW_DATA_DIR, get_api_url
+    from utils.s3_io import S3IO
+    from config.path_config import get_api_url, RAW_DATA_DIR
 
+    s3_io = S3IO()
     http_client = RequestsHttpClient(url=get_api_url())
-    storage = RawStorage(base_path=RAW_DATA_DIR)
-    extractor = Extractor(http_client=http_client, storage=storage)
+    extractor = Extractor(
+        http_client=http_client, IO_operator=s3_io, raw_data_dir=RAW_DATA_DIR
+    )
     saved_files = extractor.extract()
